07f4dfccee9a04628e6ceec96a4ae3f316ac838a
hedgehog1029 <omgredstone@gmail.com>
2014-11-30 09:57:00 +0000

    Wrote backend JSON server.

--- Cookie-Frontend/node/cookie.js
+++ Cookie-Frontend/node/cookie.js
@@ -1,25 +1,39 @@
-//!#/usr/local/bin/node
+#!/usr/local/bin/node
 
-//var request = require('request');
+var request = require('request');
 //var program = require('commander');
-//var colors = require('colors');
+var colors = require('colors');
 var http = require('http');
+var urlTools = require('url');
+var fs = require('fs');
 
 var log = function(msg) {
     console.log(" > cookie: ".magenta + msg);
 }
 
-/*
 http.createServer(function(req, res) {
+    console.log(JSON.stringify(urlTools.parse(req.url, true)));
+    //console.log(JSON.parse(JSON.stringify(urlTools.parse(req.url, true))));
+    var requestJson = JSON.parse(JSON.stringify(urlTools.parse(req.url, true)));
     
+    if ( requestJson['query'] ) {
+        //res.writeHead(200, {'Content-Type': 'text/json'});
+        if ( requestJson['query']['modpack'] ) {
+            if (fs.existsSync('./' + requestJson['query']['modpack'] + '.json')) {
+                var repo = JSON.parse(fs.readFileSync('./' + requestJson['query']['modpack'] + '.json', 'utf8'));
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end('{ "modpack": "' + requestJson['query']['modpack'] + '", "file": "' + repo['file'] + '" }');
+            } else {
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.end('Pack not found.');
+            }
+        } else {
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Hey, that tickles! Stop it!\n');
+        }
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Welcome to Cookie!\nThis page doesn\'t do much for you.\n \nInstead, it\'s used for requests for modpacks.')
+    }
 }).listen(1338, '127.0.0.1');
-*/
-
-var net = require('net');
-
-var server = net.createServer(function (socket) {
-  socket.write('Echo server\r\n');
-  socket.pipe(socket);
-});
-
-server.listen(1337, '127.0.0.1');
+log('Server running at http://127.0.0.1:1338');

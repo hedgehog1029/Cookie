8a5d237f45b6c25b1bd9981c899499dfdd95a9e2
hedgehog1029 <omgredstone@gmail.com>
2014-12-02 21:38:51 +0000

    moved a brace

--- Cookie/cookie.js
+++ Cookie/cookie.js
@@ -54,7 +54,7 @@ http.createServer(function(req, res) {
     if (md5Json['query']['folder'] && md5Json['query']['pack']) {
         //if (fs.existsSync(md5Json['query']['folder'])) {
         fs.readdir('packs/' + md5Json['query']['pack'] + '/' + md5Json['query']['folder'], function(err, files) {
-            fs.writeFile('./packs/' + md5Json['query']['pack'] + '/mods.json', '{ ', function(err) { if (err) throw err; log('overwrote ' + 'mods.json'.green); });
+            fs.writeFile('./packs/' + md5Json['query']['pack'] + '/mods.json', '', function(err) { if (err) throw err; log('overwrote ' + 'mods.json'.green); });
             if (err) log('not ok, '.red + err);
             //log('packs/' + md5Json['query']['pack'] + '/' + md5Json['query']['folder']);
             //var file;
@@ -73,7 +73,7 @@ http.createServer(function(req, res) {
 
                 stream.on('end', function () {
                     if (index == 0) {
-                        fs.appendFile('./packs/' + md5Json['query']['pack'] + '/mods.json', '"' + file + '": "' + hash.digest('hex') + '"', function(err) { 
+                        fs.appendFile('./packs/' + md5Json['query']['pack'] + '/mods.json', '{ "' + file + '": "' + hash.digest('hex') + '"', function(err) { 
                             if (err) throw err;
                         });
                     } else {
